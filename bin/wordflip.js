582fff7775beed58c609e8cc716d2057020bfd57
Howard Chan <guanhao3797@gmail.com>
2014-10-29 15:33:41 +0800

    Replaced to use commander.js instead

--- bin/wordflip.js
+++ bin/wordflip.js
@@ -1,10 +1,56 @@
 #!/usr/bin/env node
 
+/**
+ * Module dependencies.
+ */
+var program = require('commander');
 var upsidedown = require('upsidedown');
 
-if(process.argv.length < 3) {
-  console.log('Nothing to flip. ︵ /(.□. ）');
-  process.exit(1);
+/**
+ * Program options and metadata.
+ */
+program
+  .version('1.0.0')
+  .option('-v, --verbose', 'provides more information')
+  .option('-c', '--clipboard', "accesses system's clipboard");
+
+/**
+ * Program command and actions.
+ */
+program
+  .command('flip <word ...>')
+  .description('flips words like tables')
+  .action(function (word) {
+    console.log(flipWord(word));
+  });
+
+program
+  .command('unflip <word ...>')
+  .description('puts back words like tables')
+  .action(function (word) {
+    console.log(putBackWord(word));
+  })
+
+program.parse(process.argv);
+
+/**
+ * Flips word with rage face.
+ *
+ * @param {Array} words
+ * @return {String} flipped word and rage face
+ * @api private
+ */
+function flipWord(words) {
+  return '(╯°□°）╯ ' + upsidedown(words.join(' '));
 }
-var args = process.argv.splice(2);
-console.log('(╯°□°）╯ '+ upsidedown(args.join(' ')));
+
+/**
+ * Add the 'putting back' face with word.
+ *
+ * @param {Array} words
+ * @return {String} word with correct orientation and face
+ * @api private
+ */
+function putBackWord(words) {
+  return words.join(' ') + ' ノ( ゜-゜ノ)';
+}
